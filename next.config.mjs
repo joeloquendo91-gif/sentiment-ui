5054cf36ad260dd93a8b8a99c41490461086127c
joeloquendo91-gif <joel.oquendo91@gmail.com>
2026-02-23 13:59:17 -0500

    increase function timeout for batch

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
-/** @type {import('next').NextConfig} */
-const nextConfig = {
   experimental: {
     serverActions: {
       bodySizeLimit: '2mb',
